d277218e3a23acf529dba46050e3f9fa1c669f4e
alexgonca <alexandre.reportagem@gmail.com>
2017-07-19 17:46:51 -0400

    test to avoid exception during first execution

--- renamecolumns.py
+++ renamecolumns.py
@@ -12,6 +12,7 @@ class Importable:
         cols = wf_module.get_param_string('newcolnames').split(',')
         cols = [c.strip() for c in cols]
         wf_module.set_ready(notify=False)
-        table.columns = cols
+        if len(cols) != 1 or cols[0] != '':
+            table.columns = cols
         newtab = table
         return newtab
