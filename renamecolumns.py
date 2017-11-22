c04f1c5cb87fc73c531ee8ff57abf2f45a5ea07d
Jonathan Stray <jonathanstray@gmail.com>
2017-11-21 16:16:29 -0800

    Update to new API

--- renamecolumns.py
+++ renamecolumns.py
@@ -1,18 +1,6 @@
-class Importable:
-    @staticmethod
-    def __init__(self):
-        pass
-
-    @staticmethod
-    def event():
-        pass
-
-    @staticmethod
-    def render(wf_module, table):
-        cols = wf_module.get_param_string('newcolnames').split(',')
-        cols = [c.strip() for c in cols]
-        wf_module.set_ready(notify=False)
-        if len(cols) != 1 or cols[0] != '':
-            table.columns = cols
-        newtab = table
-        return newtab
+def render(table, params):
+    cols = params['newcolnames'].split(',')
+    cols = [c.strip() for c in cols]
+    if len(cols) != 1 or cols[0] != '':
+        table.columns = cols
+    return table
